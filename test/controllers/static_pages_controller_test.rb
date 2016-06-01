7bfaab6036e74fc850a6bd06ea3a42297cffe97e
Prasanth <mprasanthk27@gmail.com>
2016-06-01 20:57:29 +0530

    Add contact

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
 
-def setup do
+def setup
   @base_title = "Ruby on Rails Tutorial Sample App"
 end
 
@@ -21,7 +21,14 @@ end
   test "should get about" do
       get :about
       assert_response :success
-          assert_select "title", "About | R#{@base_title}"
+          assert_select "title", "About | #{@base_title}"
     end
+
+    test "should get contact" do
+        get :contact
+        assert_response :success
+            assert_select "title", "Contact | #{@base_title}"
+      end
+
 
 end

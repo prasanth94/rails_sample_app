2bfc58b0c9364153e92fdbbeb938ce27e83afb0f
Prasanth <mprasanthk27@gmail.com>
2016-06-01 21:17:40 +0530

    included module for title check

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+def full_title(page_title = '')
+
+  base_title = "Ruby on Rails Tutorial Sample App"
+  return base_title if page_title.empty?
+  return page_title + " | " + base_title
+end
+
 end

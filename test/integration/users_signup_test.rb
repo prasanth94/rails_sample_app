72f82688afe8892550d01c1a2929fc10fb9e425d
Prasanth <mprasanthk27@gmail.com>
2016-06-11 11:16:09 +0530

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+test "invalid sign up" do
+
+  get signup_path
+  assert_no_difference "User.count" do
+    post users_path, user: {name:  "",
+                               email: "user@invalid",
+                               password:              "foo",
+                               password_confirmation: "bar" }
+
+
+  end
+
+   assert_template 'users/new'
+
+end
+
+end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -72,6 +72,12 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "email addresses should be saved as lower-case" do
+     mixed_case_email = "Foo@ExAMPle.CoM"
+     @user.email = mixed_case_email
+     @user.save
+     assert_equal mixed_case_email.downcase, @user.reload.email
+   end
 
 
 end
